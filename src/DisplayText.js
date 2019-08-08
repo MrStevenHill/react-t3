b1bae9d07af0bf37a6e9b104b22a7402ce404bfe
Steven Hill <15816322+MrStevenHill@users.noreply.github.com>
2019-08-08 15:13:52 +0100

    Add defaultProps

--- src/DisplayText.js
+++ src/DisplayText.js
@@ -2,11 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
-  text: PropTypes.string
-}
+  text: PropTypes.string,
+};
 
-const DisplayText = ({ text }) => <div>Here is some text: {text}</div>
+const defaultProps = {
+  text: "Some Text",
+};
+
+const DisplayText = ({ text }) => (
+  <div>
+    Here is some text:&nbsp;
+    {text}
+  </div>
+);
 
 DisplayText.propTypes = propTypes;
+DisplayText.defaultProps = defaultProps;
 
 export default DisplayText;
