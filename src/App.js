e5263f271be4e2b38c33c43cd29ef288a154af67
Steven Hill <15816322+MrStevenHill@users.noreply.github.com>
2019-08-08 12:27:18 +0100

    Base code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import DisplayText from './DisplayText';
+
+function App() {
+  return (
+    <div>
+      <h1>React Training Session 3</h1>
+      <DisplayText text="Some Text" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/DisplayText.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const DisplayText = ({ text }) => <div>Here is some text: {text}</div>
+
+export default DisplayText;
