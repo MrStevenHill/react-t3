40a37ba405c8fda93ca8c2d5c6ba9100d50019cd
Steven Hill <15816322+MrStevenHill@users.noreply.github.com>
2019-08-08 12:41:56 +0100

    Add PropTypes

--- src/DisplayText.js
+++ src/DisplayText.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  text: PropTypes.string
+}
 
 const DisplayText = ({ text }) => <div>Here is some text: {text}</div>
+
+DisplayText.propTypes = propTypes;
 
 export default DisplayText;
