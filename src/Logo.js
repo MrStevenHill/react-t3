52c2131ce3dca01fd5e93d64e5f1cb8f86e52294
Steven Hill <15816322+MrStevenHill@users.noreply.github.com>
2019-08-08 13:17:34 +0100

    Fix linting issues

--- src/Logo.js
+++ src/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 
-const Logo = () => <img src={logo} alt='logo' width='100px' />
+const Logo = () => <img src={logo} alt="logo" width="100px" />;
 
 export default Logo;
